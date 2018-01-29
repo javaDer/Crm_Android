7b61d1da7347f9e3f4f9e4ca196a30869dc0e339
Javaer <fa20091001@163.com>
2018-01-29 17:36:49 +0800

    写入mongodb

--- Crm_Android.py
+++ Crm_Android.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from flask import Flask, request
-from flask.ext.pymongo import PyMongo
-from config import MongoDB
-import chardet
+# from flask.ext.pymongo import PyMongo
 import urllib
+import json
+import datetime
+import pymongo
 
 app = Flask(__name__)
 # 实例化数据库配置，可以直接一行解决
-mongo = PyMongo(app)
+# mongo = PyMongo(app)
+client = pymongo.MongoClient(host='localhost', port=27017)
+collection = client.position.expire
+collection.create_index([("time", pymongo.ASCENDING)], expireAfterSeconds=66)
 
 
 @app.route('/')
@@ -19,13 +23,23 @@ def hello_world():
 @app.route('/v1/api/', methods=['GET', 'POST'])
 def save():
     paramers = request.get_data()
-    print (urllib.unquote(paramers))
-    geographical = mongo.db.geographical
-    geographical.insert({"username": "swper", "password": "123456"})
-    if geographical:
-        return "用户已经存在！"
-    else:
-        return "Added User!"
+    data = urllib.unquote(paramers)
+    insert_data = data.split("=")[1]
+    _data = json.loads(insert_data)
+    collection.insert({
+        "latitude": _data['latitude'],
+        "longitude": _data['longitude'],
+        "moblie": _data['moblie'],
+        "address": _data['address'],
+        "time": datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+    })
+
+
+@app.route("/v1/get")
+def getcollection():
+    results = collection.find({'moblie': '17693522393'})
+    _result = json.dumps(str(results))
+    print _result
 
 
 if __name__ == '__main__':
